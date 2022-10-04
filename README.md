4a3b56adaf97a32c36084263a3611e43e34ec20a
EUNUDADDY <zioiz8781@gmail.com>
2022-10-04 15:01:41 +0900

    Create customer.js

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Management System
+
+초인의 리액트 연습임.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    img: 'https://placeimg.com/64/64/1',
+    name: '김은우',
+    birthday: '191203',
+    gender: '남아',
+    job: '어린이',
+  },
+  {
+    id: 2,
+    img: 'https://placeimg.com/64/64/2',
+    name: '김동훈',
+    birthday: '811019',
+    gender: '남자',
+    job: '대디',
+  },
+  {
+    id: 3,
+    img: 'https://placeimg.com/64/64/3',
+    name: '문혜숙',
+    birthday: '900701',
+    gender: '여자',
+    job: '마미',
+  },
+];
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              img={c.img}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+        {/* <Customer
+          id={customers[0].id}
+          img={customers[0].img}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        />
+        <Customer
+          id={customers[1].id}
+          img={customers[1].img}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
+        />
+        <Customer
+          id={customers[2].id}
+          img={customers[2].img}
+          name={customers[2].name}
+          birthday={customers[2].birthday}
+          gender={customers[2].gender}
+          job={customers[2].job}
+        /> */}
+      </div>
+    );
+  }
 }
-
-export default App;
